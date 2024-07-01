7b8057db8864149fdc98e0caa1e6590ae953a281
lynx <dishanshakya@gmail.com>
2024-07-01 22:03:38 +0545

    gg

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-require('dotenv').config();
+const test = require('dotenv').config();
 
 const pool = new Pool({
     user: process.env.PG_USER,
@@ -8,5 +8,6 @@ const pool = new Pool({
     password:  process.env.PG_PASSWORD,
     port:  process.env.PG_PORT
 });
+
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const pg = require('pg')
-const {pool} = require('./database/db')
+const pool = require('./database/db')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+app.post('/login', async (req, res) => {
+    const {email, password} = req.body
+    const user = await pool.query('select email,password from users where email=$1', [email])
+    if (user.rows.length && password == user.rows[0].password){
+        res.sendStatus(200)
+        console.log('success')
+    }
+    else {
+        res.sendStatus(400)
+        console.log('fail')
 
-app.get('/', (req, res) => {
-    res.send('hello nigga').status(200)
+    }
 })
+
 app.listen(4000, ()=> {
     console.log('Server listening on port 4000...')
 })
